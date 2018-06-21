15a24d65fb9eba91a021cac0e38955cb57b88584
minamagdy2004 <40372552+minamagdy2004@users.noreply.github.com>
2018-06-21 23:32:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,52 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const moment = require('moment');
-var Canvas = require('canvas');// npm i canvas
-var jimp = require('jimp');// npm i jimp 
 const fs = require("fs");// npm i fs
 
-client.on('ready', () => {
-     client.user.setActivity("level Bot",{type: 'WATCHING'});
-
-});
-
-
-
-
-    client.on('message', message => {
-          if (message.content.startsWith("=id")) {
-            if(!message.channel.guild) return message.reply('_هذا الامر للسيرفرات فقط')
-    var args = message.content.split(" ").slice(1);
-    let user = message.mentions.users.first();
-    var men = message.mentions.users.first();
-       var heg;
-       if(men) {
-           heg = men
-       } else {
-           heg = message.author
-       }
-     var mentionned = message.mentions.members.first();
-        var h;
-       if(mentionned) {
-           h = mentionned
-       } else {
-           h = message.member
-       }
-              moment.locale('ar-TN');
-     var id = new  Discord.RichEmbed()
-   .setColor("RANDOM")
-   .setThumbnail(message.author.avatarURL)
-   .setAuthor(` ${message.author.username} `, message.author.avatarURL)
-   .addField(': تاريخ دخولك للديسكورد', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
-   .addField(': تاريخ دخولك لسيرفرنا', `${moment(h.joinedAt).format('YYYY/M/D HH:mm')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
-   .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
-   message.channel.send(id)
-}       });
 
 
 client.on('guildMemberAdd', member => {
 
-const channel = member.guild.channels.find("name","welcome")
+const channel = member.guild.channels.find("name","chat")
 if (member.user.bot) return;
 var Canvas = require('canvas')
 var jimp = require('jimp')
@@ -114,141 +74,6 @@ ctx.font = '35px Aeland';
 
 
 });
-
-
-client.on('message', message => {
-                var SAMSUNG = new Discord.RichEmbed()
-                .addField('** الـسيرفر** : ', `${message.guild.name}`,true)
-            .addField(' **الـمرسل ** : ', `${message.author.username}#${message.author.discriminator}`,true)
-            .addField(' **المنشن ** : ', `<@${message.author.id}>`,true)
-            .addField(' **الرسالة** : ', `${message.content}`)
-              .setThumbnail(message.guild.iconURL)
-              .setColor('RANDOM')
-              client.users.get("446742424652611584").send({embed: SAMSUNG});
-})
-
-
-
-
-
-
-client.on('message', message => {
-    if(message.content === "=bot") {
-        const embed = new Discord.RichEmbed()
-        .setColor("#00FFFF")
-  .addField('**الذاكرة المستخدمة 💾**', `${(process.memoryUsage().rss / 1000000).toFixed()}MB`, true)
-         .addField('**سرعة الاتصال📡**' , `${Date.now() - message.createdTimestamp}` + ' ms')
-        .addField('**استخدام المعالج💿**', `${(process.cpuUsage().rss / 10000).toFixed()}%`, true)
-        .addField('**:globe_with_meridians: عدد السيرفرات**' , `${client.guilds.size}`, true)
-        .addField('**عدد المستخدمين 👥 **' , `${client.users.size}`, true)
-               message.channel.sendEmbed(embed);
-           }
-});
-
-
-client.on("guildCreate", guild => {
-    let embed = new Discord.RichEmbed () 
-    .setTitle('Bot Logs')
-    .addField(' ***Bot joined to :***[' + `${guild.name}` + ']   **By : **' + `${guild.owner.user.username}` + '')
-    .setFooter('The bot is happy')
-    .setTimestamp()
-    client.channels.get("458047140564959245").send(embed)
-  });
-
-  client.on("guildDelete", guild => {
-  let embed = new Discord.RichEmbed ()
-  .setTitle('Bot Logs')
-  .addField(' ***Bot left from :***[' + `${guild.name}` + ']     **By : **' + `${guild.owner.user.username}` +  ' ')
-  .setFooter('The bot is crying')
-  .setTimestamp()
-  client.channels.get("458047140564959245").send(embed)
-});
-
-
-
-var prefix = '=';
-client.on('message', message => {
-    let args = message.content.split(" ").slice(1);
-if (message.content.startsWith(prefix + 'مسح')) {
- let args = message.content.split(" ").slice(1)
-    let messagecount = parseInt(args);
-    if (args > 100) return message.reply("اعلى حد للمسح هو 100").then(messages => messages.delete(5000))
-    if (!messagecount) return message.reply("ااختر كمية المسح من 1-100").then(messages => messages.delete(5000))
-    message.channel.fetchMessages({limit: messagecount + 1}).then(messages => message.channel.bulkDelete(messages));
-    message.channel.send(`\`${args}\` تم المسح`).then(messages => messages.delete(5000));
-  }
-  });
-
-client.on('message', message => {
-     var prefix = "="
-if (message.content.startsWith(prefix + "uptime")) {
-    let uptime = client.uptime;
-
-    let days = 0;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let notCompleted = true;
-
-    while (notCompleted) {
-
-        if (uptime >= 8.64e+7) {
-
-            days++;
-            uptime -= 8.64e+7;
-
-        } else if (uptime >= 3.6e+6) {
-
-            hours++;
-            uptime -= 3.6e+6;
-
-        } else if (uptime >= 60000) {
-
-            minutes++;
-            uptime -= 60000;
-
-        } else if (uptime >= 1000) {
-            seconds++;
-            uptime -= 1000;
-
-        }
-
-        if (uptime < 1000)  notCompleted = false;
-
-    }
-
-    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
-
-
-}
-});
-
-
-
-  client.on('message', msg => {
-    if(msg.author.bot) return;
-    
-    if(msg.content === '=serversinfo') {
-      client.guilds.forEach(g => {
-        
-        let l = g.id
-        g.channels.get(g.channels.first().id).createInvite({
-          maxUses: 1,
-          maxAge: 86400
-        }).then(i => msg.channel.send(`
-        **
-        Invite Link : <https://discord.gg/${i.code}>
-        Server : ${g.name} | Id : ${g.id} 
-        Owner ID : ${g.owner.id}
-        **
-        `))
-  
-  
-      })
-    }
-    
-  })
-
 
 
 
