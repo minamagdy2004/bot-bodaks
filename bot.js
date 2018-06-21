aa0bc8937227521525e5e162f5ea7a9413f7c359
minamagdy2004 <40372552+minamagdy2004@users.noreply.github.com>
2018-06-22 01:06:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,23 +2,21 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const client = new Discord.Client();
 const prefix = '+'
-client.on('ready', () => {
-  console.log('╔[════════════════════════════════════]╗');
-  console.log('')
-  console.log('            ╔[════════════]╗')
-  console.log('              Bot Is Online')
-  console.log('            ╚[════════════]╝')
-  console.log('')
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log('')
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log('')
-  console.log('╚[════════════════════════════════════]╝')
-});
-client.on('ready', () => {
-     client.user.setActivity("+help",{type: 'WATCHING'})
+client.on('ready', function(){
+    var ms = 100000 ;
+    var setGame = [`+help `,`${client.guilds.size}`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
+    }, ms);100000
 
 });
 
