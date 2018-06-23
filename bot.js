9958695f6bf5ce610fb037e719f0c7cfbc361bd4
minamagdy2004 <40372552+minamagdy2004@users.noreply.github.com>
2018-06-24 02:04:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3598,4 +3598,84 @@ client.on('message', msg => {
 }
 });
 
+
+
+client.on('guildCreate', guild => {
+  client.channels.get("460217176461148160").send(`**تم اضافه بوت في سيرفر ✅
+Server name: __${guild.name}__
+Server owner: __${guild.owner}__**`)
+});
+
+
+
+
+
+      client.on('guildMemberAdd', member => {
+      const welcomer =  member.guild.channels.find('name', 'welcome');//اسم الروم الي يرحب فيه
+
+      var Canvas = require('canvas')
+      var jimp = require('jimp')
+
+      const w = ['./img/w1.png',
+      './img/blue.png'];
+
+              let Image = Canvas.Image,
+                  canvas = new Canvas(401, 202),
+                  ctx = canvas.getContext('2d');
+              ctx.patternQuality = 'bilinear';
+              ctx.filter = 'bilinear';
+              ctx.antialias = 'subpixel';
+              ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
+              ctx.shadowOffsetY = 2;
+              ctx.shadowBlur = 2;
+              fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+                  if (err) return console.log(err)
+                  let BG = Canvas.Image;
+                  let ground = new Image;
+                  ground.src = Background;
+                  ctx.drawImage(ground, 0, 0, 401, 202);
+
+      })
+
+                      let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".gif" : member.user.displayAvatarURL;
+                      jimp.read(url, (err, ava) => {
+                          if (err) return console.log(err);
+                          ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                              if (err) return console.log(err);
+
+                              
+                              let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.drawImage(ava, 152, 27, 95, 95);
+
+                                                      //wl
+                              ctx.font = '20px Arial Bold';
+                              ctx.fontSize = '15px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+                                                         ctx.fillText(member.user.username, 200, 154);
+
+                              //NAME
+                              ctx.font = '20px Arial';
+                              ctx.fontSize = '28px';
+                              ctx.fillStyle = "#FFFFFF";
+                              ctx.textAlign = "center";
+                                    ctx.fillText(`Member namber${member.guild.memberCount} `
+                              , 200, 190);
+
+ welcomer.sendFile(canvas.toBuffer())
+
+
+
+      })
+      })
+      });
+
+
+
+
+
+
+
 client.login("NDU5NDY1NjQ4MTQyMjg2ODU4.Dg_LgA.l3XbxHzQv0-smbbxwKux_lNkGsA");
