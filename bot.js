60dfa04390f98c3a7e323dddf26d5b172dddb6db
minamagdy2004 <40372552+minamagdy2004@users.noreply.github.com>
2018-06-23 15:02:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3678,7 +3678,7 @@ client.on('message', msg => {
 
 
 
-client.login("NDU4MzkzMzMzNzU3MTE2NDM3.Dg_Feg.yzpK8ZXxo-QPZ6dQcmK-D7ae0Ek");
+client.login("NDU5NDY1NjQ4MTQyMjg2ODU4.Dg_H4w.N_vQgYRHoo7PQ3fpMFwOwh6xeOM");
 
 
 
