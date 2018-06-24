efa95b1d5199769473a486b5b239ace8a4c5dd64
minamagdy2004 <40372552+minamagdy2004@users.noreply.github.com>
2018-06-25 02:01:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,65 +6,6 @@ var Canvas = require('canvas');// npm i canvas
 var jimp = require('jimp');// npm i jimp 
 const fs = require("fs");// npm i fs
 const yt = require('ytdl-core');
-const db = require('quick.db')
-const prefix = '+';
-client.on('message', async message => {
-   if(message.content.startsWith(prefix + "credits")) {
- let args = messa+ge.content.split(' ').slice(1);
-
-var user = message.mentions.users.first() || message.author;
-        
-        var balance = await db.fetch(`userBalance_${user.id}`)
-        
-        if (balance === null) balance = 50;
-        
-        var embed = new Discord.RichEmbed()
-        .setTitle('Coin Balance')
-        .setDescription(`${user.username}, **انت لديك:\n:dollar: $${balance}**`)
-        .setColor('#ffffff')
-        .setFooter('Requested By ' + message.author.tag, message.author.avatarURL)
-        message.channel.send(embed)
-
-}
-});
-const ms = require('ms')
-client.on('message', async message => {
-   if(message.content.startsWith(prefix + "daily")) {
-    let cooldown = 8.64e+7,
-    amount = 250
-
-    let lastDaily = await db.fetch(`lastDaily_${message.author.id}`)
-    try {
-    db.fetch(`userBalance_${message.member.id}`).then(bucks => {
-    if(bucks == null){
-        db.set(`userBalance_${message.member.id}`, 50)}
-
-    else if (lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0) {
-        let timeObj = ms(cooldown - (Date.now() - lastDaily))
-
-        let lastDailyEmbed = new Discord.RichEmbed()
-        .setAuthor(`Next Daily`)
-        .setColor('#ffffff')
-        .setDescription(`You sucessfully collected this, you must wait to collect next dily. Time Left: **${timeObj}**!`)
-        .setFooter('Requested By ' + message.author.tag, message.author.avatarURL)
-        message.channel.send(lastDailyEmbed)
-    } else {
-        db.set(`lastDaily_${message.author.id}`, Date.now());
-        db.add(`userBalance_${message.member.id}`, amount).then(i => {
-          var discord = require('discord.js')
-          var embed = new Discord.RichEmbed()
-          .setTitle('Todays Daily')
-          .setDescription(` you received your :dollar:$${amount}`)
-          .setColor('#ffffff')
-          .setFooter('Requested By ' + message.author.tag, message.author.avatarURL)
-          message.channel.send(embed);
-        })}
-    })} catch(err) {console.log(err)}
-}
-});
-
-
-
 
 
 
@@ -1711,8 +1652,74 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+
+if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+  wins: 0,
+  loses: 0,
+  game: 0,
+
+  };
+  if (message.author.bot) return;
 
 
+if(!message.channel.guild) return;
+    let userData = points[message.author.id];
+
+if (message.content.startsWith(prefix + 'point')) {
+let pointss = userData.points
+try {
+                            pointss = shortNumber(pointss);
+                        } catch (error) {
+                            pointss = 0;
+                        }
+                        let wins = userData.wins
+try {
+                            wins = shortNumber(wins);
+                        } catch (error) {
+                            wins = 0;
+                        }
+                        let loses = userData.loses
+try {
+                            loses = shortNumber(loses);
+                        } catch (error) {
+                            loses  = 0;
+                        }
+                         let games = userData.game
+try {
+                            games = shortNumber(games);
+                        } catch (error) {
+                            games  = 0;
+                        }
+    let embed = new Discord.RichEmbed()
+    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+    .setColor('#000000')
+    .setDescription(`**S Bot
+
+:white_check_mark: عدد الفوز : ${wins}
+:x: عدد الخسارة: ${loses}
+:label:التقاط: ${pointss}
+:video_game: عدد مرات اللعب: ${games}**` , '');
+    message.channel.sendEmbed(embed)
+  }
+  fs.writeFile("./point.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  })
+
+});
+
+ =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- 
+client.on('message',function(message) {
+    let messageArray = message.content.split(" ");
+    let args = messageArray[1];
+   if(message.content.startsWith(prefix + "counting")) {
+       if(!args) return message.reply('ℹ ``اختر رقم``');
+       let i;
+       for (i = 0; i < `${parseInt(args) + 1}`; ++i)
+       message.channel.send(i)
+   }
+});
 
 
 
